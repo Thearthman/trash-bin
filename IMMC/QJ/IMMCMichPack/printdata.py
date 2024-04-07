75609ccf033c963f34e8a8e0142a78e035cf86b7
Thearthman <61790014+Thearthman@users.noreply.github.com>
2024-04-07 20:16:20 +0800

    Delete IMMC directory

--- IMMC/QJ/IMMCMichPack/printdata.py
+++
@@ -1,6 +0,0 @@
-def printdata (sortedList, avgSortedList, adList):
-    print(sortedList)
-    print(avgSortedList.sum())
-    print(adList[len(adList) - 10:len(
-        adList) - 1])
-    print(min(adList))
